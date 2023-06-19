23aa3f255e409701ebd332daeb14e46f6fb0122c
Gustavo Castro <gustavoprasca@hotmail.com>
2023-06-18 19:01:59 -0500

    the project is renamed

--- src/main/java/com/co/softworld/ReactiveApiApplication.java
+++ src/main/java/com/co/softworld/ReactiveApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NameApplication {
+public class ReactiveApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NameApplication.class, args);
+		SpringApplication.run(ReactiveApiApplication.class, args);
 	}
 
 }
