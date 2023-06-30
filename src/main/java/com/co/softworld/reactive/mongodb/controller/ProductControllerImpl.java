1168c9ef4f2f779a11892ebac78b32bd747e8e92
Gustavo Castro Prasca <gcastro@novopayment.com>
2023-06-30 08:22:44 -0500

    response entity added

--- src/main/java/com/co/softworld/reactive/mongodb/controller/ProductControllerImpl.java
+++ src/main/java/com/co/softworld/reactive/mongodb/controller/ProductControllerImpl.java
@@ -3,6 +3,7 @@ package com.co.softworld.reactive.mongodb.controller;
 import com.co.softworld.reactive.mongodb.model.Product;
 import com.co.softworld.reactive.mongodb.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -16,25 +17,25 @@ public class ProductControllerImpl implements IProductController{
 
     @Override
     @GetMapping
-    public Flux<Product> findAll() {
+    public Mono<ResponseEntity<Flux<Product>>> findAll() {
         return productService.findAll();
     }
 
     @Override
     @GetMapping("/{id}")
-    public Mono<Product> findById(@PathVariable String id) {
+    public Mono<ResponseEntity<Product>> findById(@PathVariable String id) {
         return productService.findById(id);
     }
 
     @Override
     @PostMapping
-    public Mono<Product> save(@RequestBody Product product) {
+    public Mono<ResponseEntity<Product>> save(@RequestBody Product product) {
         return productService.save(product);
     }
 
     @Override
     @DeleteMapping
-    public Mono<Void> delete(Product product) {
+    public Mono<ResponseEntity<Void>> delete(Product product) {
         return productService.delete(product);
     }
 }
